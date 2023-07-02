5003e349a06f01d7a2efcbd60ca3b2dfa3450687
Takahiro <tkhr_621@docomo.ne.jp>
2023-07-03 00:27:13 +0900

    format

--- src/index.ts
+++ src/index.ts
@@ -23,12 +23,7 @@ export const parse = (jestCode: string): t.Expression[] => {
     }
   }
 
-  // traverse 関数は、AST のノードを再帰的に走査し、指定したノードタイプにマッチするノードが見つかるたびに
-  // 指定されたコールバック関数（この場合は traverseCallExpressions 関数）を呼び出す
-  // traverseCallExpressions 関数は、CallExpression ノードが見つかるたびに呼び出される
   traverse(ast, {
-    // コールバック関数には現在のノードの情報が渡される、
-    // それを受け取るためのパラメータが  `path: NodePath<t.CallExpression>`
     CallExpression: traverseCallExpressions
   })
 
